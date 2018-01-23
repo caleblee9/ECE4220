96ed48c8c3c71d52fc8832ea22f3715c270f1e8e
Caleb <caleblee3@gmail.com>
2018-01-23 11:44:09 -0600

    LEDs on and off and speaker:

---
+++ Lab1/onOffLab1.c
@@ -0,0 +1,38 @@
+/*
+ ============================================================================
+ Name        : onOffLab1.c
+ Author      : Caleb Lee
+ Version     :
+ Copyright   : 
+ Description : Turns LEDs off and on every second
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <wiringPi.h>
+
+#define LED1 7		//define ports
+#define LED2 21
+
+int main(void) {
+
+	wiringPiSetup();
+
+	pinMode(LED1, OUTPUT);		//LEDs set to output
+	pinMode(LED2, OUTPUT);
+	char input;
+	while(1) {
+		
+		digitalWrite(LED1, HIGH);	//LED1 on
+		sleep(1);
+		digitalWrite(LED1, LOW);	//LED1 off
+		digitalWrite(LED2, HIGH);	//LED2 on
+		sleep(1);
+		digitalWrite(LED2, LOW);	//LED1 off
+		
+		
+	}
+	return EXIT_SUCCESS;
+}
---
+++ Lab1/speakerLab1.c
@@ -0,0 +1,102 @@
+/*
+ ============================================================================
+ Name        : onOffLab1.c
+ Author      : Caleb Lee
+ Version     :
+ Copyright   : 
+ Description : Speaker square wave
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <wiringPi.h>
+
+#define BUTTON1 27	//define ports
+#define BUTTON2 0
+#define BUTTON3 1
+#define BUTTON4 24
+#define BUTTON5 28
+#define SPEAKER 22
+
+
+int main(void) {
+	wiringPiSetup();
+	
+
+	pinMode(BUTTON1, INPUT);
+	pinMode(BUTTON2, INPUT);
+	pinMode(BUTTON3, INPUT);
+	pinMode(BUTTON4, INPUT);
+	pinMode(BUTTON5, INPUT);
+
+	pinMode(SPEAKER, OUTPUT);
+
+
+	printf("Please enter a number 1 - 5: ");
+	int num;
+	scanf("%d", &num);
+	while(num < 1 || num > 5) {
+		printf("Invalid option, please enter 1 -5: ");
+		scanf("%d", &num);
+	}
+	while(1) {
+		switch(num) {
+			case 1:
+				if(digitalRead(BUTTON1)) {
+					digitalWrite(SPEAKER, HIGH);
+					usleep(400);
+					digitalWrite(SPEAKER, LOW);
+					usleep(400);
+				} else {
+					break;
+				}
+			case 2:
+				if(digitalRead(BUTTON2)) {
+					digitalWrite(SPEAKER, HIGH);
+					usleep(400);
+					digitalWrite(SPEAKER, LOW);
+					usleep(400);		
+				} else {
+					break;
+				}
+
+			case 3:
+				if(digitalRead(BUTTON3)) {
+					digitalWrite(SPEAKER, HIGH);
+					usleep(400);
+					digitalWrite(SPEAKER, LOW);
+					usleep(400);		
+				} else {
+					break;
+				}
+
+			case 4:
+				if(digitalRead(BUTTON4)) {
+					digitalWrite(SPEAKER, HIGH);
+					usleep(400);
+					digitalWrite(SPEAKER, LOW);
+					usleep(400);		
+				} else {
+					break;
+				}
+
+			case 5:
+				if(digitalRead(BUTTON5)) {
+					digitalWrite(SPEAKER, HIGH);
+					usleep(400);
+					digitalWrite(SPEAKER, LOW);
+					usleep(400);		
+				} else {
+					break;
+				}
+
+		}
+
+
+
+	}
+
+	return EXIT_SUCCESS;
+}
