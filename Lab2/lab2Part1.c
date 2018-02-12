ccb7c114196c5aba3984f473efca58de9fe6d5a5
Caleb <caleblee3@gmail.com>
2018-02-12 15:26:31 -0600

    added comments

--- Lab2/lab2Part1.c
+++ Lab2/lab2Part1.c
@@ -5,7 +5,7 @@
 
 typedef struct t1 {
 	int val;
-	int *counter;
+	int *counter;		//thread 1 struct for multiple input parameters
 	int *array;
 	int size;
 } T1;
@@ -14,43 +14,43 @@ typedef struct t1 {
 
 
 typedef struct t2{
-	int val;
-	int *counter;
-	int *row;
-	int size;
+	int val;		//val user enters in command line
+	int *counter;		//thread 2 struct
+	int *row;	//numbers in the row
+	int size;	//size of the row
 } T2;
 
 
 
 typedef struct t3{
-	int val;
-	int *counter;
-	int *col;
-	int size;
+	int val;		//val to check for
+	int *counter;		//counter of number of occurrences
+	int *col;		//numbers in the columns
+	int size;		//size of the columns
 } T3;
 
 typedef struct t4{
-	int val;
-	int num;
-	int *counter;
+	int val;		//number to compare to
+	int num;		//number we're checking
+	int *counter;		//counter for number of occurrences
 } T4;
 
 
-void *entireSearch(void *);
-void *rowSearch(void *);
-void *colSearch(void *);
-void *elemCheck(void *);
+void *entireSearch(void *);	//entire matrix search
+void *rowSearch(void *);	//searches rows
+void *colSearch(void *);	//searches cols
+void *elemCheck(void *);	//checks every element
 
 
 
 
 int main(int argc, char *argv[]) {
-	pthread_t thread1;
-	int num = atoi(argv[2]);
+	pthread_t thread1;		//create the thread
+	int num = atoi(argv[2]);	//number to check for
 	
 	FILE *fp;
 	if((fp = fopen(argv[1], "r")) == NULL ) {
-		printf("Cannot open file, please check to make sure file exists\n");
+		printf("Cannot open file, please check to make sure file exists\n");	//make sure file can be opened
 		return 0;
 	}
 
@@ -58,12 +58,12 @@ int main(int argc, char *argv[]) {
 	fscanf(fp, "%d", &rows);
 	int cols = 0;
 	fscanf(fp, "%d", &cols);
-	int size = rows * cols;
+	int size = rows * cols;		//get number of rows, columns, and number of elements = size
 
-	int *array = malloc(sizeof(int) * size);
+	int *array = malloc(sizeof(int) * size);	//store values in array
 	int i;
 	for(i = 0; i < size; i++) {
-		fscanf(fp, "%d", (array + i));
+		fscanf(fp, "%d", (array + i));		//scan them in
 	}
 
 
@@ -72,46 +72,46 @@ int main(int argc, char *argv[]) {
 ----------------------------------------------------------------
 -----------------------ENTIRE MATRIX----------------------------
 ----------------------------------------------------------------
-*/
-	T1 thr1;
+*/	
+	T1 thr1;		//first thread
 	thr1.val = num;
 	thr1.array = array;
 	thr1.size = size;
 	thr1.counter = malloc(sizeof(int));
 	(*thr1.counter) = 0;
-	clock_t begin = clock();
-	pthread_create(&thread1, NULL, (void *)&entireSearch, &thr1);
-	pthread_join(thread1, NULL);
-	clock_t end = clock();
-	double time_spent = (double)(end - begin);
+	clock_t begin = clock();		//start clock
+	pthread_create(&thread1, NULL, (void *)&entireSearch, &thr1);//run the search of entire matrix
+	pthread_join(thread1, NULL);			//wait until thread1 is complete
+	clock_t end = clock();				//end timer
+	double time_spent = (double)(end - begin);	//get total time
 
-	system("clear");
+	system("clear");		
 	printf("----Entire Matrix----\n");
-	printf("Times found: %d\n", *thr1.counter);
-	printf("Time taken: %lf ms\n", time_spent);
+	printf("Times found: %d\n", *thr1.counter);	//print results
+	printf("Time taken: %.2lf ms\n", time_spent);
 	free(thr1.counter);
 /*
 --------------------------------------------------------------------
 ---------------------------EACH ROW---------------------------------
 --------------------------------------------------------------------
 */
-	pthread_t thread2[rows];
+	pthread_t thread2[rows];	//number of threads per row
 	T2 thr2;
 	thr2.val = num;
 	thr2.size = cols;
 	int counter = 0;
-	thr2.counter = &counter;
-	int *row = malloc(sizeof(int) * cols);
+	thr2.counter = &counter;	//using pass by reference to store values
+	int *row = malloc(sizeof(int) * cols);	//store values in row. number of elements is equal to the number of columns
 	int j;
 	int h = 0;
 	begin = clock();
 	for(i = 0; i < rows; i++) {
 		for(j = 0; j < cols; j++) {
 			*(row + j) = *(array + h);
-			h++;	
+			h++;	//holds array location
 		}				
 		thr2.row = row;
-		pthread_create(&thread2[i], NULL, (void *)&rowSearch, &thr2);
+		pthread_create(&thread2[i], NULL, (void *)&rowSearch, &thr2);	//for each row, create a thread and search that row
 		pthread_join(thread2[i], NULL);		
 	}
 	end = clock();
@@ -119,8 +119,8 @@ int main(int argc, char *argv[]) {
 
 
 	printf("\n\n----Each Row----\n");
-	printf("Times found: %d\n", counter);
-	printf("Time taken: %lf ms\n", time_spent);
+	printf("Times found: %d\n", counter);		//print results
+	printf("Time taken: %.2lf ms\n", time_spent);
 	free(row);
 	
 
@@ -134,7 +134,7 @@ int main(int argc, char *argv[]) {
 	thr3.val = num;
 	thr3.size = rows;
 	counter = 0;
-	thr3.counter = &counter;
+	thr3.counter = &counter;	//pass by reference
 	int *col = malloc(sizeof(int) * rows);
 	h = 0;
 	int k = 0;
@@ -143,20 +143,20 @@ int main(int argc, char *argv[]) {
 		h = k;
 		for(j = 0; j < rows; j++) {
 			*(col + j) = *(array + h);
-			h = h + 15;	
+			h = h + rows;	//each of the columns holds the position in the array incrememnted by the number of rows
 		}				
 		thr3.col = col;
-		pthread_create(&thread3[i], NULL, (void *)&colSearch, &thr3);
+		pthread_create(&thread3[i], NULL, (void *)&colSearch, &thr3);	//for each column, create a thread
 		pthread_join(thread3[i], NULL);
-		k++;	
+		k++;	//which column we're on
 	}
 	end = clock();
 	time_spent = (double)(end - begin);
 
 
 	printf("\n\n----Each Column----\n");
-	printf("Times found: %d\n", counter);
-	printf("Time taken: %lf ms\n\n\n", time_spent);
+	printf("Times found: %d\n", counter);			//print stats
+	printf("Time taken: %.2lf ms\n\n\n", time_spent);
 	free(row);
 /*
 ------------------------------------------------------------------
@@ -166,12 +166,13 @@ int main(int argc, char *argv[]) {
 	pthread_t thread4[size];
 	T4 thr4;
 	thr4.val = num;
+	thr1.fp = fp;
 	counter = 0;
 	thr4.counter = &counter;
 	begin = clock();
 	for(i = 0; i < size; i++) {
 		thr4.num = *(array + i);
-		pthread_create(&thread4[i], NULL, (void *)&elemCheck, &thr4);
+		pthread_create(&thread4[i], NULL, (void *)&elemCheck, &thr4);	//for each element create a thread
 		pthread_join(thread4[i], NULL);	
 	}
 	end = clock();
@@ -179,8 +180,8 @@ int main(int argc, char *argv[]) {
 
 
 	printf("\n\n----Each Element----\n");
-	printf("Times found: %d\n", counter);
-	printf("Time taken: %lf ms\n\n\n", time_spent);
+	printf("Times found: %d\n", counter);		//print stats
+	printf("Time taken: %.2lf ms\n\n\n", time_spent);
 
 
 	free(array);
@@ -194,7 +195,7 @@ void *entireSearch(void *t1){
 	T1 *thr1 = (T1 *)t1;
 	int i;
 	for(i = 0; i < thr1->size; i++) {
-		if(*(thr1->array + i) == thr1->val) {
+		if(*(thr1->array + i) == thr1->val) {	//if array contains the value, increment the counter
 			(*thr1->counter)++;
 		}
 	}
