30aa48da60834670dea96c86e65c5ccfd3513717
Caleb <caleblee3@gmail.com>
2018-03-13 11:40:47 -0500

    part 3 of the lab

--- Lab4/lab4.c
+++ Lab4/lab4.c
@@ -13,16 +13,42 @@
 
 
 #define BTN1 27
-unsigned char val;	//val that we're reading in
-struct timeval tv;
-sem_t mutex;
+
+
+typedef struct data {
+	struct timeval tvRT;
+	unsigned char prevval;
+	struct timeval tvPrev;
+	unsigned char nextval;
+	struct timeval tvNext;
+	
+} Data;
+unsigned char val;
+struct timeval tvGPS;	//for time stamps
+struct timeval tvRT;
+sem_t mutex;		//semaphore
+
 void *getData(void *);
 void *rt_Event(void *);
+void *dynThread(void *);
+
+
 int main() {
-	int a = fork();
+	int N_pipe2[2];
+	if(pipe(N_pipe2) < 0) {
+		printf("N_pipe2 creation error\n");
+		exit(-1);
+	}
+	int a;
+	if((a = fork()) < 0) {
+		printf("Fork error\n");
+		exit(-1);
+	}
 	wiringPiSetup();	//setup Wiring PI
 	sem_init(&mutex, 0 , 1);	//initialize semaphore
 	pinMode(BTN1, INPUT); //set PinMode for the button
+
+
 	if (a != 0){
 		int N_pipe1;	//named pipe
 
@@ -33,33 +59,48 @@ int main() {
 		}
 
 		pthread_t thread0;
-		pthread_create(&thread0, NULL, getData, NULL);
+		pthread_create(&thread0, NULL, getData, N_pipe2);
 		while(1) {		//loop to get GPS data and store it in buffer as well as time stamp
 			if(read(N_pipe1, &val, sizeof(val)) < 0) {
 				printf("N_pipe1 pipe read error\n");
 				return 0;
 			}
-			gettimeofday(&tv, NULL);
-		
+			gettimeofday(&tvGPS, NULL);	
 		}
 		pthread_join(thread0, NULL);
 	} else {
 		pthread_t thread1;
-		pthread_create(&thread1, NULL, rt_Event, NULL);
+		pthread_create(&thread1, NULL, rt_Event, &N_pipe2);
 		pthread_join(thread1, 0);
 	}
 	return 0;
 }
 void *getData(void * ptr) {
+	int *N_pipe2 = (int *) ptr;
 	while(1) {
 		usleep(250000);
-		printf("GPS Data: %d\n", (int)val);
-		printf("Seconds: %lf Microseconds: %ld\n", (double)tv.tv_sec, tv.tv_usec);
-			
+		pthread_t dyn;
+		Data five;
+		int b;
+		five.prevval = val;
+		gettimeofday(&five.tvPrev, NULL);
+		if(read(N_pipe2[0], &five.tvRT, sizeof(tvRT)) < 0) {
+				printf("N_pipe2 read error\n");
+				exit(-1);
+		} else {
+			if((b = fork()) < 0) {
+				printf("Fork error\n");
+				exit(-1);
+			}
+			if(b == 0) {
+				pthread_create(&dyn, NULL, dynThread, &five);
+			}		
+		}	
 	}
 	pthread_exit(0);
 }
 void *rt_Event(void * ptr) {
+	int *N_pipe2 = (int *) ptr;
 	uint64_t num_periods = 0;
 	int timer_fd = timerfd_create(CLOCK_MONOTONIC, 0);
 
@@ -74,8 +115,11 @@ void *rt_Event(void * ptr) {
 		while(1){
 			read(timer_fd, &num_periods, sizeof(num_periods));
 			if(check_button()) {
-				gettimeofday(&tv, NULL);
-				printf("REAL TIME EVENT SECONDS: %lf MICROSECONDS %ld\n", (double) tv.tv_sec, tv.tv_usec);
+				gettimeofday(&tvRT, NULL);
+				if(write(N_pipe2[1], &tvRT, sizeof(tvRT)) != sizeof(tvRT)) {
+					printf("Pipe time stamp write error\n");
+					exit(-1);
+				}
 			}
 			clear_button();
 			if(num_periods > 1) {
@@ -84,4 +128,16 @@ void *rt_Event(void * ptr) {
 			}		
 		}
 		pthread_exit(0);		
+}
+void *dynThread(void * ptr) {
+	Data *five = (Data *) ptr;
+	while(1) {
+		if(val != read(N_pipe1, &val, sizeof(val))) {
+			five->nextval = val;
+			gettimeofday(five->tvNext, NULL);
+			break;	
+		}
+	}
+	
+	
 }
