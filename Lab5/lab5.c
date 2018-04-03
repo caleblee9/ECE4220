d494a9281bb8437492b1bb29baf2504eabf57482
Caleb <caleblee3@gmail.com>
2018-04-03 11:41:56 -0500

    updated

--- Lab5/lab5.c
+++ Lab5/lab5.c
@@ -8,48 +8,113 @@
 #include <net/if.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-char *getIP(); 
+char *getIP();
+typedef struct master {
+	char *ip; //client ip
+	char *name;	//client name
+	int num; //rand number generated
+	char *lastVal; //of IP
+} Master;
 int main(int argc, char *argv[]) {
+	if(argc < 2) {
+		printf("usage: %s port\n", argv[0]);
+		return 0;
+	}
+	srand(time(NULL));
+	int rnd;
+	Master m;	//to hold master information
+	m.ip = malloc(sizeof(char) * 32);
+
+	m.num = 0;	//set master number to 0
+	char *ip = malloc(sizeof(char) * 32); //store parsed IPs
+	int mNum; //store parsed values
+	const char s[2] = " "; //delimiter
+	char *fC; //to check for "#"
+	char *lastVal; //received last val of ip
 	int n;
-	char buffer[40];
+	char buffer[40]; //message with size of 40
 	socklen_t fromlen;
-	int sock = socket(AF_INET, SOCK_DGRAM, 0);
+	int sock = socket(AF_INET, SOCK_DGRAM, 0); //create the socket
 	if(sock < 0) {
 		printf("Error opening socket\n");
 		exit(-1);
 	}
 	struct sockaddr_in server;
 	struct sockaddr_in addr;
-	bzero(&server, sizeof(server));
-	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = INADDR_ANY;
-	server.sin_port = htons(atoi(argv[1]));
+	bzero(&server, sizeof(server)); //sets all values of server to 0
+	server.sin_family = AF_INET; //Internet domain
+	server.sin_addr.s_addr = INADDR_ANY;	//IP address on running machine
+	server.sin_port = htons(atoi(argv[1]));	// get the port number from the user args
 	
 
-	if(bind(sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
+	if(bind(sock, (struct sockaddr *)&server, sizeof(server)) < 0) { //binds socket to IP address of host and the user entered port number
 		printf("error binding socket\n");
 		exit(-1);
 	}
 	
 	int broadEnable = 1;
-	int ret = setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &broadEnable, sizeof(broadEnable));
+	int ret = setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &broadEnable, sizeof(broadEnable)); //set socket to allow broadcast
 	if(ret < 0 ) {
 		printf("Error setting socket to broadcast\n");
 		exit(-1);
 	}
-	fromlen = sizeof(struct sockaddr_in);
+	fromlen = sizeof(struct sockaddr_in); //size of socket structure
 
 	while(1) {
-		bzero(buffer, 40);
-		n = recvfrom(sock, buffer, 40, 0, (struct sockaddr *)&addr, &fromlen);
+		bzero(buffer, 40); //refresh buffer
+		n = recvfrom(sock, buffer, 40, 0, (struct sockaddr *)&addr, &fromlen); //receive messages from clients
 		if (n < 0) {
 			printf("Receiving error\n");
 			exit(-1);
-		}
-		printf("Received message: %s", buffer);
-		addr.sin_addr.s_addr = inet_addr("192.168.1.255");
+		}			
+		printf("Received message: %s", buffer); //print the message received
 
+		if(strcmp(buffer, "WHOIS\n") == 0)  {
+			if(m.num == 0) {
+				printf("No master assigned yet\n");
+			} else {
+				printf("IP %s is the master\n", m.ip);
+			}
+		}
+
+		if(strcmp(buffer, "VOTE\n") == 0) {
+			m.num = 0;
+			rnd = (rand() % 10 + 1);
+			bzero(buffer, 40);
+			sprintf(buffer, "# %s %d\n", getIP(), rnd);
+			addr.sin_addr.s_addr = inet_addr("128.206.19.255"); 	//set IP to broadcast (.255)
+			n = sendto(sock, buffer, 40, 0, (struct sockaddr *)&addr, fromlen);
+			if (n < 0 ) {
+				printf("Error sento\n");
+			}
+
+		}
+		fC = strtok(buffer, s);
+		if(strcmp(fC, "#") == 0) {
+			ip = strtok(NULL, s);
+			mNum = atoi(strtok(NULL, s));
+			if(mNum > m.num) {
+				m.num = mNum;
+				strcpy(m.ip, ip);
+			} else if(mNum == m.num) {
+				lastVal = strtok(ip, ".");
+				while(lastVal != NULL) {
+					lastVal = strtok(NULL, ".");
+				}
+				m.lastVal = strtok(m.ip, ".");
+				while(m.lastVal != NULL) {
+					m.lastVal = strtok(NULL, ".");
+				}
+				printf("%s %s\n", lastVal, m.lastVal);
+				if(atoi(lastVal) > atoi(m.lastVal)) {
+					m.num = mNum;
+					strcpy(m.ip, ip);
+				}
+			}	
+		}
 	}
+	free(ip);
+	free(m.ip);
         return 0;
 }
 char *getIP() {	
